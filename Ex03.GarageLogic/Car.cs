f2b752e534fba61da38301b476a3ff24d8c31b31
furanimus <babitskyd@gmail.com>
2019-08-22 22:27:42 +0300

    test

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    public abstract class Car : Vehicle //Maybe Internal?
+    public abstract class Car : Vehicle //Maybe Internal??
     {
         protected enum eColor
         {
